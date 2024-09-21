084847d5acedaa30c942836a77fda20b60b7ef99
Tim-s06 <timsandro.nguyen@gmail.com>
2024-09-21 11:44:27 +0200

    Added functions for the basic math operations and variables for the calculator operation consisting of two numbers and an operator

---
+++ script.js
@@ -0,0 +1,15 @@
+let numberOne = 0;
+let numberTwo = 0;
+let operator = "";
+function add(a,b) {
+    return a+b;
+}
+function subtract(a,b) {
+    return a-b;
+}
+function multiply(a,b) {
+    return a*b;
+}
+function divide(a,b) {
+    return a/b;
+}
