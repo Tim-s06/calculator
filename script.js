40a84d805c4b76b5b753b41ed749d66cefa261ed
Tim-s06 <timsandro.nguyen@gmail.com>
2024-09-21 11:47:48 +0200

    Added an operate function that takes an operator and 2 numbers and calls one of the basic math functions, also changed divide function to not be able to divide by zero

--- script.js
+++ script.js
@@ -11,5 +11,22 @@ function multiply(a,b) {
     return a*b;
 }
 function divide(a,b) {
+    if(b === 0) {
+        return "ERROR"
+    }
     return a/b;
+}
+function operate(firstNum, secondNum, operator) {
+    if(operator == "+") {
+        add(firstNum,secondNum);
+    }
+    if(operator == "*") {
+        multiply(firstNum,secondNum);
+    }
+    if(operator == "-") {
+        subtract(firstNum,secondNum);
+    }
+    if(operator == "/") {
+        divide(firstNum, secondNum);
+    }
 }
